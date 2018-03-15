e7f2161544975fa8fb118b326e2995975a4933a5
David Dellsperger <david.dellsperger@gmail.com>
2018-03-14 22:53:37 -0500

    Adding message to prevent rate-limiting (#4)
    Fixes issue with rate limiting on slack's side by telling the user that it cannot perform the task

--- src/app/tasks/webhooks.py
+++ src/app/tasks/webhooks.py
@@ -9,9 +9,13 @@ class ThonkifyEndpoint(RequestHandler):
         logging.debug(self.request.POST)
         text_to_translate = self.request.POST.get('text', 'ThOnK')
         translated_text = thonkify.thonkify(text_to_translate)
+        if len(translated_text) > 4000:
+            logging.debug("Text too long, reporting characters 3999 - 4001: " + translated_text[3999:4001])
+            translated_text = 'I\'m afraid I can\'t do that: <@' + self.request.POST.get('user_id') + '>'
         self.response.headers['Content-Type'] = 'application/json'
         data = {
             'response_type': 'in_channel',
+            'link_names': 1,
             'text': translated_text
         }
         self.response.out.write(json_encode(data))
